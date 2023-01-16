feb5802c07af41631b2c87e2d66429404c32f607
Jay <jay_on_business@outlook.com>
2023-01-16 23:06:24 +0800

    20230116

---
+++ day24-code/src/cn/mycloudway/demo/MapDemo4.java
@@ -0,0 +1,17 @@
+package cn.mycloudway.demo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapDemo4 {
+    public static void main(String[] args) {
+        Map<String, String> map = new HashMap<>();
+
+        map.put("version", "1");
+        map.put("host", "www.baidu.com");
+        map.put("status", "200");
+        map.put("msg", "ok");
+
+        map.forEach((key, value) -> System.out.println(key + ": " + value));
+    }
+}
