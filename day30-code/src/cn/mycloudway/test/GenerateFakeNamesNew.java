c625282bb9e170935184afd4b247a99f6fd9dcd4
Jay <jay_on_business@outlook.com>
2023-02-06 21:58:01 +0800

    20230206

--- day30-code/src/cn/mycloudway/test/GenerateFakeNamesNew.java
+++ day30-code/src/cn/mycloudway/test/GenerateFakeNamesNew.java
@@ -1,31 +1,26 @@
 package cn.mycloudway.test;
 
+import cn.hutool.core.util.ReUtil;
+import cn.hutool.http.HttpUtil;
+
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.URLConnection;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Random;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.*;
 
-public class GenerateFakeNames {
+public class GenerateFakeNamesNew {
     public static void main(String[] args) throws IOException {
         String familyNameUrl = "https://hanyu.baidu.com/shici/detail?pid=0b2f26d4c0ddb3ee693fdb1137ee1b0d&from=kg0";
         String boyNameUrl = "http://www.haoming8.cn/baobao/10881.html";
         String girlNameUrl = "http://www.haoming8.cn/baobao/7641.html";
 
-        String webFamilyNames = webCrawler(familyNameUrl);
-        String webBoyNames = webCrawler(boyNameUrl);
-        String webGirlNames = webCrawler(girlNameUrl);
+        String familyNameContent = HttpUtil.get(familyNameUrl);
+        String boyNameContent = HttpUtil.get(boyNameUrl);
+        String girlNameContent = HttpUtil.get(girlNameUrl);
 
-        ArrayList<String> tempFamilyNameList = getData(webFamilyNames, "(.{4})(，|。)", 1);
-        ArrayList<String> tempBoyNameList = getData(webBoyNames, "([\\u4E00-\\u9FA5]{2})(、|。)", 1);
-        ArrayList<String> tempGirlNameList = getData(webGirlNames, "(.. ){4}..", 0);
+        List<String> tempFamilyNameList = ReUtil.findAll( "(.{4})(，|。)", familyNameContent, 1);
+        List<String> tempBoyNameList = ReUtil.findAll( "([\\u4E00-\\u9FA5]{2})(、|。)", boyNameContent, 1);
+        List<String> tempGirlNameList = ReUtil.findAll( "(.. ){4}..", girlNameContent, 0);
 
         ArrayList<String> familyNameList = new ArrayList<>();
         for (String names : tempFamilyNameList) {
@@ -50,7 +45,7 @@ public class GenerateFakeNames {
         HashSet<String> boyNames = getBoyNames(familyNameList, boyNameList, 60);
         HashSet<String> girlNames = getGirlNames(familyNameList, girlNameList, 60);
 
-        BufferedWriter bw = new BufferedWriter(new FileWriter("day30-code/names.txt"));
+        BufferedWriter bw = new BufferedWriter(new FileWriter("day30-code/newNames.txt"));
         for (String name : boyNames) {
             bw.write(name);
             bw.newLine();
@@ -61,33 +56,6 @@ public class GenerateFakeNames {
         }
 
         bw.close();
-    }
-
-    public static String webCrawler(String urlAddress) throws IOException {
-        StringBuilder sb = new StringBuilder();
-        URL url = new URL(urlAddress);
-        URLConnection conn = url.openConnection();
-        InputStreamReader isr = new InputStreamReader(conn.getInputStream());
-        int ch;
-        while ((ch = isr.read()) != -1) {
-            sb.append((char) ch);
-        }
-
-        isr.close();
-
-        return sb.toString();
-    }
-
-    public static ArrayList<String> getData(String str, String regex, int index) {
-        ArrayList<String> list = new ArrayList<>();
-
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(str);
-        while (matcher.find()) {
-            list.add(matcher.group(index));
-        }
-
-        return list;
     }
 
     public static HashSet<String> getBoyNames(ArrayList<String> familyNameList, ArrayList<String> boyNameList, int count) {
